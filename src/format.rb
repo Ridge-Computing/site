c394c735865abb3106091d4ae7f2228fefdb168d
eJi <veryeji@gmail.com>
2021-02-07 12:17:44 -0500

    Remove non Gemoji and non Kramdown
    This needs more work. It's not good enough rn.

--- src/format.rb
+++ src/format.rb
@@ -14,33 +14,34 @@ def emojify(content)
     end
 end
 
-def sup(content)
-    content.to_str.gsub(/(?<!\\)\^\((.+)(?<!\\)\)/) do |match|
-        '<sup markdown="1">' << $1 << '</sup>'
-    end
-end
-
-def sub(content)
-    content.to_str.gsub(/(?<!\\)_\((.+)(?<!\\)\)/) do |match|
-        '<sub markdown="1">' << $1 << '</sub>'
-    end
-end
-
-def strike(content)
-    content.to_str.gsub(/(?<!\\)~~(.+)(?<!\\)~~/) do |match|
-        '<s markdown="1">' << $1 << '</s>'
-    end
-end
-
-def under(content)
-    content.to_str.gsub(/(?<!\\)__(.+)(?<!\\)__/) do |match|
-        '<u markdown="1">' << $1 << '</u>'
-    end
-end
+# def sup(content)
+#     content.to_str.gsub(/(?<!\\)\^\((.+)(?<!\\)\)/) do |match|
+#         '<sup markdown="1">' << $1 << '</sup>'
+#     end
+# end
+#
+# def sub(content)
+#     content.to_str.gsub(/(?<!\\)_\((.+)(?<!\\)\)/) do |match|
+#         '<sub markdown="1">' << $1 << '</sub>'
+#     end
+# end
+#
+# def strike(content)
+#     content.to_str.gsub(/(?<!\\)~~(.+)(?<!\\)~~/) do |match|
+#         '<s markdown="1">' << $1 << '</s>'
+#     end
+# end
+#
+# def under(content)
+#     content.to_str.gsub(/(?<!\\)__(.+)(?<!\\)__/) do |match|
+#         '<u markdown="1">' << $1 << '</u>'
+#     end
+# end
 
 def format(content)
     content.gsub!(/\\\\/, '&#92;')
-    content = under(strike(sup(sub(emojify(content)))))
+#     content = under(strike(sup(sub(emojify(content)))))
+    content = emojify(content)
     content.gsub!(/\\\^/, '^').gsub!(/\\~/, '~').gsub!('&#92;', '\\\\')
     doc = Kramdown::Document.new(content).to_html.gsub('<ul>', '<ul style="list-style-type: disc!important; margin-left: 1em;">').gsub('<p>', '<br><p>').gsub(' markdown="1"', '')
     if doc[0..3].eql? '<br>'
